1780215200bb371dd94d2e0e1f4e15fa76bed64e
Konrad Wysk <konrad4595@gmail.com>
2020-01-28 14:28:54 +0100

    initial commit

---
+++ Docker_Demo/helloworld/src/test/java/eu/busi/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.busi.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
